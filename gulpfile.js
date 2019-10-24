9cde0c04a43342b8582f2ae7572d312659cd4031
zeybek <abdulkadirzeybek95@gmail.com>
2019-10-24 16:55:32 +0300

    nunjucks eklendi- gulp file guncellendi

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,43 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const nunjucksRender = require('gulp-nunjucks-render');
 
 //compile SASS files into CSS files
-gulp.task('sass', function() {
+gulp.task('sass', () => {
   return gulp.src('docs/scss/style.scss')
     .pipe(sass())
     .pipe(gulp.dest('docs/css/'))
     .pipe(browserSync.stream());
 })
 
-// create a server and watching files
 
+gulp.task('nunjucks', () => {
+  return gulp.src('app/pages/**/*.nunjucks')
+    // Renders template with nunjucks
+    .pipe(nunjucksRender({
+      path: ['app/templates']
+    }))
+    // output files in app folder  
+    .pipe(gulp.dest('docs/'))
+    // .pipe(gulp.dest('app/'))
+    .pipe(browserSync.stream())
+});
+
+
+
+// create a server and watching files
 gulp.task('serve', function() {
 	browserSync.init({
-    server: './docs'
+    notify: false, 
+    server: 'docs/'
   });
 
   gulp.watch('docs/scss/*.scss', gulp.series('sass'));
-  gulp.watch('docs/*.html').on('change', browserSync.reload);
+  gulp.watch('app/**/*.nunjucks', gulp.parallel('nunjucks'));
+  // gulp.watch('docs/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.series(['sass', 'serve']))
+
+// gulp.task('default', gulp.series(['sass', 'serve']))
+gulp.task('default', gulp.parallel('serve', 'sass', 'nunjucks'));
